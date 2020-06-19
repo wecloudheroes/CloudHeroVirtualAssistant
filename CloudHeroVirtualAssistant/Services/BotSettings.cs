011fadb878efb25d6ca2e8c7fdd496cbbdf671c4
Sachin Suryawanshi <P00301684@capita.co.uk>
2020-06-19 13:48:05 +0530

    Add project files.

---
+++ CloudHeroVirtualAssistant/Services/BotSettings.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Solutions;
+using CloudHeroVirtualAssistant.TokenExchange;
+
+namespace CloudHeroVirtualAssistant.Services
+{
+    public class BotSettings : BotSettingsBase
+    {
+        public TokenExchangeConfig TokenExchangeConfig { get; set; }
+    }
+}
